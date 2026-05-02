84cbab0ddb0e362527f499aacbc16af957dd1ccb
Astras | Hub <yansaigre@gmail.com>
2026-05-02 17:49:50 -0400

    Update main.lua

--- WindUI-main/dist/main.lua
+++ WindUI-main/dist/main.lua
@@ -6870,22 +6870,24 @@ AnchorPoint=Vector2.new(0.5,0.5),
 Position=UDim2.new(0.5,0,0.5,0),
 Name="Frame",
 ThemeTag={
-ImageColor3="Text", 
+ImageColor3="Text",
 },
 },{
 ae.NewRoundFrame(99,"Squircle",{
-Name="Frame", 
+Name="Frame",
 Size=UDim2.new(ar,0,1,0),
 ImageTransparency=.1,
-
+-- CAMBIO AQUÍ: Solo la barra de progreso es VERDE
 ImageColor3=Color3.fromRGB(0,255,106), 
 },{
 ae.NewRoundFrame(99,"Squircle",{
 Size=UDim2.new(0,ak.Window.NewElements and(al.ThumbSize*2)or(al.ThumbSize+2),0,ak.Window.NewElements and(al.ThumbSize+4)or(al.ThumbSize+2)),
 Position=UDim2.new(1,0,0.5,0),
 AnchorPoint=Vector2.new(0.5,0.5),
-
-ImageColor3=Color3.fromRGB(255,255,255), 
+-- LA BOLITA SE MANTIENE IGUAL (Blanca por defecto o según tema)
+ThemeTag={
+ImageColor3="SliderThumb", 
+},
 Name="Thumb",
 },{
 ae.NewRoundFrame(99,"Glass-1",{
